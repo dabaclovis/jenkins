281b2c6739b4549e7ae89b0dde4c5fe988ddab42
clovis daba <dabaclovis@yahoo.com>
2022-10-16 03:22:37 -0400

    testing jenkins

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main
+{
+    public static void main(String[] args) 
+    {
+        String y = "hello world from clovis \n";
+        String x = "Clovis is a cameroonian";
+        System.out.println(y
+        + ' ' + x);
+    }
+}
